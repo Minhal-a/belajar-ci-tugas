2fb6f7701d150a176da60eae40832927521c48d8
Minhal3 <111202415763@mhs.dinus.ac.id>
2026-06-15 15:19:02 +0700

    first commit

---
+++ app/Views/components/sidebar.php
@@ -0,0 +1,38 @@
+<!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link <?= uri_string() == '' ? '' : 'collapsed' ?>" href="<?= base_url() ?>">
+          <i class="bi bi-grid"></i>
+          <span>Home</span>
+        </a>
+      </li><!-- End Home Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link <?= uri_string() == 'keranjang' ? '' : 'collapsed' ?>" href="<?= base_url('keranjang') ?>">
+          <i class="bi bi-cart"></i>
+          <span>Keranjang</span>
+        </a>
+      </li><!-- End Keranjang Nav -->
+
+      <?php if(session()->get('role') == 'admin'): ?>
+      <li class="nav-item">
+        <a class="nav-link <?= uri_string() == 'produk' ? '' : 'collapsed' ?>" href="<?= base_url('produk') ?>">
+          <i class="bi bi-box"></i>
+          <span>Produk</span>
+        </a>
+      </li><!-- End Produk Nav -->
+      <?php endif; ?>
+
+      <li class="nav-item">
+        <a class="nav-link <?= uri_string() == 'profile' ? '' : 'collapsed' ?>" href="<?= base_url('profile') ?>">
+          <i class="bi bi-person"></i>
+          <span>Profile</span>
+        </a>
+      </li><!-- End Profile Page Nav -->
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
---
+++ app/Views/v_profile.php
@@ -0,0 +1,20 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+<div class="pagetitle">
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Home</a></li>
+      <li class="breadcrumb-item active">Profile</li>
+    </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<section class="section">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Profile</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, ad impedit? Hic magnam sapiente aliquam obcaecati delectus ratione et harum tempore enim itaque praesentium laudantium rerum, ipsam debitis non exercitationem ipsa mollitia quia ab numquam iste, unde placeat officia alias. Facere, reiciendis obcaecati rerum veritatis blanditiis in, ipsa dolorem porro saepe quidem laboriosam quibusdam! Nam maiores, reiciendis illum sequi facilis quam fugiat similique incidunt quidem delectus possimus explicabo! Velit impedit totam beatae ut accusamus iste eius odit maiores, quos sit dolorem nostrum ad suscipit itaque. Maiores facere ipsum est maxime ipsam quasi sequi, porro numquam, iusto aspernatur expedita sit odit corporis, quam ratione eaque quis explicabo ab totam debitis illum. Culpa adipisci sint perferendis facilis sequi dolore possimus, odio veritatis. Veritatis, ex quas nostrum odit labore cupiditate obcaecati? Nobis, delectus distinctio temporibus modi sequi suscipit eius nulla quo recusandae perspiciatis at dolorum, harum odit dolorem quaerat, vero error voluptatem sapiente necessitatibus rem! Impedit itaque natus sunt cumque dicta non doloremque.</p>
+    </div>
+  </div>
+</section>
+<?= $this->endSection() ?>
